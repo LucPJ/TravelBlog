a41cfda396d8fc5d156e6b67c0de7844cd12e0df
Christin Schade <christinabbe90gmail.com>
2022-11-04 15:08:11 +0100

    fetch data and first style

--- src/Components/CategoryBlogCard.js
+++ src/Components/CategoryBlogCard.js
@@ -2,15 +2,13 @@ import React from "react";
 
 
 export default function CategoryBlogCard({ blogCardData }) {
-  
-
-  
   return(
     <div className="category-blog-card">
-      <img src={blogCardData.cardImage.file.url} alt={blogCardData.cardImage.description}/>
+      <img src={blogCardData.cardImage.fields.file.url} alt={blogCardData.cardImage.fields.description}/>
       { blogCardData.cardTitle }
       { blogCardData.cardText }
       { blogCardData.cardLink }
+      { blogCardData.cardShareLink}
 
     </div>
   )
