eb8732b117dcd1e4be4b5ab363a9ec8048c60e22
Marco Kadlubski <marco.kdlbsk@mailbox.org>
2022-11-08 17:35:13 +0100

    fixes

--- src/Components/Main/Landingpage/Landingpage.js
+++ src/Components/Main/Landingpage/Landingpage.js
@@ -1,37 +1,79 @@
 import React, { useState, useEffect } from "react";
-import { apiBase, accessToken } from "../../../Helper/ApiConstants";
+import { Link } from "react-router-dom";
+import { client } from "../../../Helper/ApiConstants";
+import { v4 as uuidv4 } from "uuid";
 
 export default function Landingpage() {
   const [stageImgData, setStageImgData] = useState();
   const [stageImgUrl, setStageImgUrl] = useState();
   const [categoryData, setCategoryData] = useState();
+  const [categoryCards, setCategoryCards] = useState();
   const [isLoading, setIsLoading] = useState(false);
 
-  async function getStageData() {
+  useEffect(() => {
     setIsLoading(true);
-    const response = await fetch(
-      `${apiBase}/entries?content_type=categories&select=fields.headerImage&${accessToken}`
-    );
-    const data = await response.json();
-    setStageImgData(data);
-  }
+    client
+      .getEntries({ content_type: "categories", select: "fields.headerImage" })
+      .then((response) => setStageImgData(response.items[0]))
+      .catch((err) => console.error(err));
+
+    client
+      .getEntries({ content_type: "categories" })
+      .then((response) => setCategoryData(response.includes.Entry))
+      .catch((err) => console.error(err));
+  }, []);
 
   useEffect(() => {
     if (stageImgData) {
-      setStageImgUrl(stageImgData.includes.Asset[0].fields.file.url);
+      setStageImgUrl(stageImgData.fields.headerImage.fields.file.url);
+      setIsLoading(false);
     }
-  }, [stageImgData]);
+    if (categoryData) {
+      setCategoryCards(
+        categoryData.map((categoryCard) => {
+          console.log(categoryCard.fields);
+          return (
+            <div key={uuidv4()} className="card">
+              <img
+                src={`https:${categoryCard.fields.categoryImage.fields.file.url}`}
+                alt={`https:${categoryCard.fields.categoryImage.fields.title}`}
+              />
+              <div className="card-details">
+                <h2>{categoryCard.fields.categoryTitle}</h2>
+                <p>
+                  {categoryCard.fields.previewText.content[0].content[0].value}
+                </p>
+                <div className="links">
+                  <Link to={categoryCard.fields.cta} className="cta">
+                    Zur Kategorie
+                  </Link>
+                  <div className="more">
+                    <a href="#"></a>
+                    <a href="#"></a>
+                    <a href={categoryCard.fields.shareLink}>
+                      <img src="" alt="" />
+                    </a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          );
+        })
+      );
+      setIsLoading(false);
+    }
+  }, [stageImgData, categoryData]);
 
-  useEffect(() => {
-    getStageData();
-  }, []);
+  if (isLoading) {
+    return <h1>Loading...</h1>;
+  }
 
   return (
     <>
       <div className="stageImg">
         <img src={stageImgUrl} alt="" />
       </div>
-      <div className="cardsContainer"></div>
+      <div className="cardsContainer">{categoryCards}</div>
     </>
   );
 }
--- src/Components/blogcards/CategoryBlogCard.js
+++ src/Components/blogcards/CategoryBlogCard.js
@@ -1,15 +1,18 @@
 import React from "react";
 
-
 export default function CategoryBlogCard({ blogCardData }) {
-  return(
+  console.log("Card", blogCardData);
+  return (
     <div className="category-blog-card">
-      <img src={blogCardData.cardImage.fields.file.url} alt={blogCardData.cardImage.fields.description}/>
-      { blogCardData.cardTitle }
-      { blogCardData.cardText }
-      { blogCardData.cardLink }
-      { blogCardData.cardShareLink}
-
+      <h1>Card</h1>
+      <img
+        src={blogCardData.cardImage.fields.file.url}
+        alt={blogCardData.cardImage.fields.description}
+      />
+      {blogCardData.cardTitle}
+      {blogCardData.cardText}
+      {blogCardData.cardLink}
+      {blogCardData.cardShareLink}
     </div>
-  )
+  );
 }
