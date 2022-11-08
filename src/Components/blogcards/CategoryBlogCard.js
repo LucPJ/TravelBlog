b74ab4527409bb6f7fba30da2b546de15106c400
Christin Schade <christinabbe90gmail.com>
2022-11-08 18:01:41 +0100

    text

--- src/Components/blogcards/CategoryBlogCard.js
+++ src/Components/blogcards/CategoryBlogCard.js
@@ -2,6 +2,9 @@ import React from "react";
 
 
 export default function CategoryBlogCard({ blogCardData }) {
+  
+  console.log(blogCardData);
+  
   return(
     <div className="category-blog-card">
       <img src={blogCardData.cardImage.fields.file.url} alt={blogCardData.cardImage.fields.description}/>
