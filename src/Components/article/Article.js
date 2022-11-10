a7c0eb1f4385573224f3680dfedee948f38f5054
Christin Schade <christinabbe90gmail.com>
2022-11-10 17:10:47 +0100

    stlying article-side

--- src/Components/article/Article.js
+++ src/Components/article/Article.js
@@ -2,6 +2,8 @@ import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import { client } from "./Client";
 import { documentToReactComponents } from '@contentful/rich-text-react-renderer';
+import Sidebar from "./sidebar/Sidebar"
+import './styles.css';
 
 export default function Article() {
   const { slugArticle } = useParams();
@@ -34,30 +36,41 @@ export default function Article() {
             src={articles.items[0].fields.headerbild.fields.file.url}
             alt={articles.items[0].fields.headerbild.fields.description}
           />
-          <div className="article-blog">
-            <h1>{articles.items[0].fields.title}</h1>
-            <p>
-              {articles.items[0].fields.description.content[0].content[0].value}
-            </p>
-            <p>
-            {documentToReactComponents(articles.items[0].fields.articleText)}
-            </p>
+
+          <div className="sidebar-and-article">
+
+            <div className="sidebar">
+              <Sidebar />
+            </div>
+           
+            <div className="article-container">
+
+              <div className="article-blog">
+                <h1>{articles.items[0].fields.title}</h1>
+                <p className="bold-text">
+                  {articles.items[0].fields.description.content[0].content[0].value}
+                </p>
+                <p></p>
+                {documentToReactComponents(articles.items[0].fields.articleText)}
+              </div>
+
+              <div className="author-container">
+                <img
+                  className="author-image"
+                  src={
+                    articles.items[0].fields.author.fields.authorAvatar.fields.file
+                      .url
+                  }
+                  alt={
+                    articles.items[0].fields.author.fields.authorAvatar.fields
+                      .description
+                  }
+                />
+                <p className="author-name">Artikel von {articles.items[0].fields.author.fields.name}</p> 
+              </div>
+            </div>
+           </div> 
           </div>
-          <div className="author-container">
-            <img
-              className="author-image"
-              src={
-                articles.items[0].fields.author.fields.authorAvatar.fields.file
-                  .url
-              }
-              alt={
-                articles.items[0].fields.author.fields.authorAvatar.fields
-                  .description
-              }
-            />
-            <p>{articles.items[0].fields.author.fields.name}</p>
-          </div>
-        </div>
       )}
     </div>
   );
