c1e3ad20573d5b941494565cdf8c1792e243f508
Lucy Palencia <lucy.palencia@gmail.com>
2022-11-11 16:06:34 +0100

    BackToTopButton

--- src/Components/Main/index.js
+++ src/Components/Main/index.js
@@ -1,11 +1,13 @@
 import React from "react";
 import MainRoutes from "../Routing/MainRoutes";
 import Landingpage from "./Landingpage/Landingpage";
+import BackToTopButton from "./../Assets/backToTopButton";
 
 export default function Main() {
   return (
     <main>
       <MainRoutes />
+      <BackToTopButton />
     </main>
   );
 }
