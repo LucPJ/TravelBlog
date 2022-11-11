c0292a389e72158616d7406ab5cc7a5fe350d05e
Lucy Palencia <lucy.palencia@gmail.com>
2022-11-11 14:03:09 +0100

    footer

--- src/Components/article/Article.js
+++ src/Components/article/Article.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
-import { client } from "./client";
+import { client } from "./Client";
 import "./article.css";
 import { documentToReactComponents } from "@contentful/rich-text-react-renderer";
 import Map from "./Map";
